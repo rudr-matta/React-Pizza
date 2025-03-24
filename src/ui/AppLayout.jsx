c6141ed66113ff726a081623d70a55b160195d02
Rudr Matta <159267290+rudr-matta@users.noreply.github.com>
2025-03-24 22:40:31 +0530

    Update AppLayout.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
-import Loader from "./LOader";
+import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
